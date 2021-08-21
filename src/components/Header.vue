4dc43164b0a3e535468eceb0bf7fb11365101d58
Ukdong Lee <41932978+dontBlamestorming@users.noreply.github.com>
2021-08-21 11:27:28 +0900

    task tracker 디자인 및 구조

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button @click="onClick()" text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  components: { Button },
+  props: {
+    title: String,
+  },
+  methods: {
+    onClick() {},
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
