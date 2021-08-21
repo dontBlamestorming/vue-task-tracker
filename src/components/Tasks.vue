ee71db1295d333813f6adea4fa07ab7db266464b
Ukdong Lee <41932978+dontBlamestorming@users.noreply.github.com>
2021-08-21 14:59:10 +0900

    add toggleReminder && short cut of emit

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,10 @@
 <template>
   <div :key="task.id" v-for="task in tasks">
-    <Task @delete-task="$emit('delete-task', task.id)" :task="task" />
+    <Task
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      :task="task"
+    />
   </div>
 </template>
 
@@ -13,7 +17,7 @@ export default {
   props: {
     tasks: Array,
   },
-  emits: ["delete-task"],
+  emits: ["delete-task", "toggle-reminder"],
 };
 </script>
 
